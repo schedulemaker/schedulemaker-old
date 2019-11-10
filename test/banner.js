29b994ca985c782d815a4eb51d7226e12f5c4c11
Paul Hutchings <36493057+paulhutchings@users.noreply.github.com>
2019-11-09 20:53:13 -0500

    variable renaming

--- test/banner.js
+++ test/banner.js
@@ -2,19 +2,28 @@ var assert = require('assert');
 var Banner = require('banner');
 
 describe('Banner', function() {
+  /**
+   * SETUP
+   */
   var banner = new Banner();
 
+    /**
+     * INIT()
+     */
     describe('#init(term)', function() {
       it('Should not throw an error', function(){
         assert.doesNotReject(banner.init());
       });
 
-      it('Should set the Cookie value for requests', async function() {
+      it('Should set Banner.Cookie value', async function() {
         await banner.init();
-        assert(banner.cookie);
+        assert(banner.Cookie);
       });
     });
 
+    /**
+     * GET_TERMS()
+     */
     describe('#getTerms()', function(){
       it('Should not throw an error', async function(){
         assert.doesNotReject(banner.getTerms());
@@ -26,6 +35,15 @@ describe('Banner', function() {
 
       it('Should return a non-empty array', async function(){
         assert.notDeepEqual(await banner.getTerms(), []);
+      });
+    });
+
+    /**
+     * GETSUBJECTS()
+     */
+    describe('#getSubjects()', function(){
+      it('Should not throw an error', async function(){
+        assert.doesNotReject(banner.getSubjects());
       });
     });
   });
