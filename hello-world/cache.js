bfb7111fde77f4765465ef2fff5f37ce9b214b25
Christopher Bilger <christopherbilg@gmail.com>
2019-11-21 09:47:47 -0500

    Formatted hello-world/ using jshint

--- hello-world/cache.js
+++ hello-world/cache.js
@@ -1,26 +1,31 @@
+/*jshint esversion: 8 */
+
 /**
- * @module Cache 
- * Controls the state of global objects representing connections to AWS services that can be reused throughout invokations. 
+ * @module Cache
+ * Controls the state of global objects representing connections to AWS services that can be reused throughout invokations.
  * If the variable does not exist in the namespace, it is instantiated and then reused upon subsequent executions in the same container.
  */
-module.exports = (params={S3:{endpoint: 'http://localhost:4572', s3ForcePathStyle: true, credentials: {accessKeyId: 'foo', secretAccessKey: 'bar'}},
-     DocumentClient:{endpoint: 'http://localhost:4569'}, region: 'us-east-1'}) => {
+var PARAMS = {S3: {endpoint: 'http://localhost:4572', s3ForcePathStyle: true, credentials: {accessKeyId: 'foo', secretAccessKey: 'bar'}},
+        DocumentClient: {endpoint: 'http://localhost:4569'}, region: 'us-east-1'};
 
+module.exports = (params = PARAMS) => {
+    var aws = null;
     if (!aws){
-        var aws = require('aws-sdk');
+        aws = require('aws-sdk');
     }
 
+    var documentclient = null;
     if (!documentclient){
-        var documentclient = new aws.DynamoDB.DocumentClient(params.DocumentClient);
+        documentclient = new aws.DynamoDB.DocumentClient(params.DocumentClient);
     }
 
+    var s3 = null;
     if (!s3){
-        var s3 = new aws.S3(params.S3);
+        s3 = new aws.S3(params.S3);
     }
 
-    return {
-        S3: s3,
-        DocumentClient: documentclient
-    }
-
-}
+  return {
+      S3: s3,
+      DocumentClient: documentclient
+    };
+};
