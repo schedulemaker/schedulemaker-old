9eda95d72d1988a7a210f424a542f2cdd913e55f
Paul Hutchings <36493057+paulhutchings@users.noreply.github.com>
2019-11-27 11:11:32 -0500

    updated upload script

--- lambda/scheduler/upload.sh
+++ lambda/scheduler/upload.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-zip -u function.zip index.js cache.js
+zip -u function.zip index.js cache.js node_modules/conversions node_modules/scheduler
 aws lambda update-function-code --function-name scheduler --zip-file fileb://function.zip
