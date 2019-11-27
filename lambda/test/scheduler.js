4235709b053b14976a923086e1687aeb3a7602a1
Paul Hutchings <36493057+paulhutchings@users.noreply.github.com>
2019-11-27 00:08:30 -0500

    updated modules to use strict mode

---
+++ lambda/test/scheduler.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var assert = require('assert');
+var Scheduler = require('scheduler');
+
+describe('#Scheduler', function(){
+    var scheduler = new Scheduler();
+
+    it('Should not allow time conflicts', function(){
+        
+    });
+});
