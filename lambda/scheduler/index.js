e4d558b2236d33bb7195d66c268456f9d78db03d
coizioc <36041795+coizioc@users.noreply.github.com>
2019-12-04 13:52:21 -0500

    Added support for multiple campuses in query and POST request.

--- lambda/scheduler/index.js
+++ lambda/scheduler/index.js
@@ -8,18 +8,20 @@ exports.handler = async (event, context) => {
     let DBToScheduler = cache.DBToScheduler;
     let SchedulerToUI = cache.SchedulerToUI;
     let scheduler = cache.Scheduler;
-
+    
     let params = JSON.parse(event.body);
     let courseList = params.courses;
+    let campusList = (Array.isArray(params.campus)) ? params.campus : [params.campus];
     console.log(`Received the following courses: ${courseList}`);
-
+    console.log(`Received the following campuses: ${campusList}`);
     try {
-        var data = await getSections(courseList, params.campus);
+        var data = await getSections(courseList, campusList);
     } catch (error) {
         console.log(`'Unable to get all courses from database: ${error}`);
         return {
             statusCode: '500',
             body: "Unable to process request",
+            error: error,
             headers: {
                 'Content-Type': 'application/json',
                 'Access-Control-Allow-Methods': '*',
@@ -32,6 +34,7 @@ exports.handler = async (event, context) => {
     let schedules = scheduler.createSchedules(courses);
     let results = SchedulerToUI(schedules);
     console.log(`Created ${results.length} schedules`);
+    results = results.slice(0, process.env.MAX_SCHEDULES);
     
     return {
         statusCode: '200',
--- s3/clientScripts/api.js
+++ s3/clientScripts/api.js
@@ -7,7 +7,7 @@
  *                           * days: a string containing one or more days of the week ("M", "T", "W", "R", "F")
  *                           * startTime: a number representing the start time in 24-hour format.
  *                           * endTime: a number representing the end time in 24-hour format.
- *                           * campus: a string representing the campus to do the search on, e.g. "MN" for Main.
+ *                           * campus: a array containing strings representing the campus to do the search on, e.g. ["MN"] for Main.
  * @returns {Array} an array containing all the schedules.
  */
 async function postClassTimes(params) {
@@ -15,21 +15,24 @@ async function postClassTimes(params) {
     let days = params.days || "MTWRF";
     let startTime = params.startTime || 0;
     let endTime = params.endTime || 2359;
-    let campus = params.campus || "MN";
+    let campus = params.campus || ["MN"];
+
+    let reqBody = JSON.stringify({
+        courses: courses,
+        days: days,
+        startTime: startTime,
+        endtime: endTime,
+        campus: campus
+    });
+
+    console.log(`Sending POST with body ${reqBody}`);
 
     var response = await fetch('https://m9uqnix64d.execute-api.us-east-2.amazonaws.com/dev', {
        method: 'post',
        headers: {
-           'Accept': 'application/json',
-           'Content-Type': 'application/json'
+           'Accept': 'application/json'
         },
-        body: JSON.stringify({
-            "courses": courses,
-            "days": days,
-            "startTime": startTime,
-            "endtime": endTime,
-            "campus": campus
-        })
+        body: reqBody
     });
 
     var results = await response.json();
